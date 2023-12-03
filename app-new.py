3f6b31677aad7bd5a3080d91e03e1a376055e60a
Camden Thomson <thomsoncamden@gmail.com>
2023-12-02 18:08:43 -0600

    Improved Reddit processing time and added more informative processing messages

--- app-new.py
+++ app-new.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, jsonify, render_template, session
+from flask import Flask, request, jsonify, render_template
 from threading import Thread
-import time
 
 # Import your utility functions
 from src.utils.utilCode import get_reddit_data_for_team, extract_comments_and_scores
@@ -10,7 +9,6 @@ from src.utils.utilCode import get_match_odds, oddsScrapper
 from src.utils.utilCode import normalize_score
 from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
 from datetime import datetime
-import pandas as pd
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  
@@ -29,31 +27,34 @@ tasks = {}
 
 def long_running_task(home_team, away_team, task_id):
     try:
-        # Fetch Reddit data for both teams
+        # Fetch Reddit data for both teams and extract comment bodies
+        tasks[task_id] = {'status': 'pending', 'message': f'Scraping {home_team} social media posts'}
         home_team_data = get_reddit_data_for_team(home_team)
-        away_team_data = get_reddit_data_for_team(away_team)
+        comment_bodies_home, _ = extract_comments_and_scores(home_team_data)
 
-        # Extracting comment bodies 
-        comment_bodies_Home, _ = extract_comments_and_scores(home_team_data)
-        comment_bodies_Away, _ = extract_comments_and_scores(away_team_data)
+        tasks[task_id] = {'status': 'pending', 'message': f'Scraping {away_team} social media posts'}
+        away_team_data = get_reddit_data_for_team(away_team)
+        comment_bodies_away, _ = extract_comments_and_scores(away_team_data)
 
         # Sentiment Analysis
-        home_team_sentiments = batch_predict(comment_bodies_Home, tokenizer, sentiment_model)
-        away_team_sentiments = batch_predict(comment_bodies_Away, tokenizer, sentiment_model)
+        tasks[task_id] = {'status': 'pending', 'message': f'Analyzing {home_team} sentiment'}
+        home_team_sentiments = batch_predict(comment_bodies_home, tokenizer, sentiment_model)
 
+        tasks[task_id] = {'status': 'pending', 'message': f'Analyzing {away_team} sentiment'}
+        away_team_sentiments = batch_predict(comment_bodies_away, tokenizer, sentiment_model)
 
-        # Calculate sentiment scores
-        current_date = datetime.now()
-        home_team_scores = calculate_sentiment_scores(home_team_sentiments, current_date)
-        away_team_scores = calculate_sentiment_scores(away_team_sentiments, current_date)
-
-        # Normalize sentiment scores
+        # Calculate normalized sentiment scores
+        tasks[task_id] = {'status': 'pending', 'message': f'Calculating {home_team} sentiment'}
+        home_team_scores = calculate_sentiment_scores(home_team_sentiments)
         home_team_scores = normalize_score(home_team_scores)
+
+        tasks[task_id] = {'status': 'pending', 'message': f'Calculating {away_team} sentiment'}
+        away_team_scores = calculate_sentiment_scores(away_team_sentiments)
         away_team_scores = normalize_score(away_team_scores)
 
-
-        # Getting match odds
-        odd_dict = get_match_odds(oddsScrapper(), home_team, away_team) 
+        # Fetch match odds
+        tasks[task_id] = {'status': 'pending', 'message': 'Scraping betting odds'}
+        odd_dict = get_match_odds(oddsScrapper(), home_team, away_team)
 
         # Prepare input data for prediction
         match_data = {
@@ -73,12 +74,14 @@ def long_running_task(home_team, away_team, task_id):
         print(match_data)
 
         # Prediction
+        tasks[task_id] = {'status': 'pending', 'message': 'Predicting match outcome'}
         predictions = predict_match_result(match_data, model_home, model_away, scaler)
 
         # Store the results in the session
         tasks[task_id] = {'status': 'complete', 'data': predictions}
     except Exception as e:
         tasks[task_id] = {'status': 'error', 'error': str(e)}
+        print(e.with_traceback())
 
 @app.route('/')
 def index():
@@ -89,7 +92,7 @@ def start_predict():
     home_team = request.form['home_team']
     away_team = request.form['away_team']
     task_id = f"predict_{home_team}_{away_team}"
-    tasks[task_id] = {'status': 'pending'}
+    tasks[task_id] = {'status': 'pending', 'message': 'Processing'}
 
     thread = Thread(target=long_running_task, args=(home_team, away_team, task_id))
     thread.start()
@@ -99,7 +102,7 @@ def start_predict():
 @app.route('/get-results', methods=['POST'])
 def get_results():
     task_id = request.form['task_id']
-    return jsonify(tasks.get(task_id, {'status': 'pending'}))
+    return jsonify(tasks.get(task_id, {'status': 'pending', 'message': 'Processing'}))
 
 if __name__ == '__main__':
     app.run()
